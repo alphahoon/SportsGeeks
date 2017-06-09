d80fb41e7a3f1bc14645921f1487c03adc9558d7
Jaehoon Lee <alphahoon@gmail.com>
2017-06-09 19:43:30 +0900

    task runner skeleton

---
+++ tasks.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# Keywords Update
+node crawler/Keywords/sports.js
+node crawler/Keywords/leagues.js
+node crawler/Keywords/teams.js
+
+# Trends Update
+# node crawler/trendsCaculator.js
