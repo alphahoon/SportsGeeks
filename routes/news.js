60aa254e4ec26184023ebda6c39f1e21bf900416
Jaehoon Lee <alphahoon@gmail.com>
2017-06-15 05:20:27 +0900

    news

--- routes/news.js
+++ routes/news.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.post('/', (req, res, next) => {
     const db = req.app.get('db');
+    const bodyObj = req.app.get('bodyObj');
+    const standings = db.get('standings');
 
     var json = {};
-    res.json(json);
+    standings.find({
+            team: bodyObj.team
+        })
+        .then((data) => {
+            json.status = 'OK';
+            json.articles = data.articles;
+            res.json(json);
+        })
+        .catch((err) => {
+            var msg = 'error while finding standings data.';
+            console.log(msg.red);
+            console.log(err);
+            json = {};
+            json.status = 'ERROR';
+            json.description = msg;
+            res.json(json);
+        });
 });
 
 module.exports = router;
